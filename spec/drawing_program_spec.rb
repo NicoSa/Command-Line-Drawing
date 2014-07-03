b5d42df2e794c1b889efbe3f79ba02ab904d1dbc
Nico Saueressig <nico@nicosaueressig.de>
2014-07-03 12:05:21 +0100

    one passing, one failing

--- spec/drawing_program_spec.rb
+++ spec/drawing_program_spec.rb
@@ -3,8 +3,11 @@ require 'canvas_helper_spec'
 
 describe 'Drawing Program' do
 
-	it 'creates a canvas of size x by y' do
-		expect(create_canvas("C 20 4")).to eq canvas
+	it 'creates a canvas of size 20 by 4' do
+		expect(create_canvas("C 20 4")).to eq canvas_20_by_4
 	end
 
+	it 'creates a canvas of size 10 by 5' do
+		expect(create_canvas("C 10 5")).to eq canvas_10_by_5
+	end
 end
