9310b90463b9af826fb6f5540146330c96f5d96c
Nico Saueressig <nico@nicosaueressig.de>
2014-07-04 11:03:07 +0100

    added gitignore

--- spec/helpers/horizontal_line_helper_spec.rb
+++ spec/helpers/horizontal_line_helper_spec.rb
@@ -6,13 +6,26 @@ describe 'Drawing Line Helper' do
 		@drawing_program = DrawingProgram.new
 	end
 
-	it 'sets horizontal line variables' do
-		@drawing_program.set_horizontal_line_array_variables("L 1 2 1 6")
-		puts @x1
+	it 'has set horizontal line variables' do
+		set_horizontal_line_array_variables("L 1 2 1 6")
 		expect(@x1).to eq "1"
 		expect(@y1).to eq "2"
 		expect(@x2).to eq "1"
 		expect(@y2).to eq "6"
 	end
 
+	it 'calculates horizontal line array' do
+		set_horizontal_line_array_variables("L 1 2 1 6")
+		width = "20"
+		height = "5"
+		calculate_horizontal_line_array
+		puts "---------------"
+		puts @x1
+		puts @y2
+		puts @line_array
+		puts width
+		puts height
+		puts "---------------"
+		expect(calculate_horizontal_line_array).to eq 
+	end
 end
